6a1b3e6140bd9f11131013414b6666b1e9136df7
Mohamed Galdi <contact.galdi@gmail.com>
2025-08-24 18:29:08 +0100

    m

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -63,8 +63,8 @@ class Kernel extends HttpKernel
         'signed' => \App\Http\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
-        // 'admin' => \App\Http\Middleware\AdminMiddleware::class,
-        // 'restrictAdminAccess' => \App\Http\Middleware\RestrictAdminAccess::class,
+        'admin' => \App\Http\Middleware\AdminMiddleware::class,
+        'restrictAdminAccess' => \App\Http\Middleware\RestrictAdminAccess::class,
 
     ];
 }
